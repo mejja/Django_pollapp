a4b18a48f9ae5744528a7831c74b3f3acbfaf94e
mejja <majorkiema.mk@gmail.com>
2023-10-18 18:11:39 +0300

    created questions and choices model in model,py

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+#Question model
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+#Choice model
 class Choice(models.Model):
-    Question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    Choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
