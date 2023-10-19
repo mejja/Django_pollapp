b75fb1ba5367f2fd5b054cbc500106c79d14832e
mejja <majorkiema.mk@gmail.com>
2023-10-20 00:56:35 +0300

    added url.py in the polls project folder and include url mappings

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("You're are looking at polls index" )
+
 def details(request, question_id):
     return HttpResponse("You're are looking at question %s." % question_id)
 
