2b63b81635fc0d3dc01289301b2989f4c745ceca
mejja <majorkiema.mk@gmail.com>
2023-11-02 23:10:36 +0300

    removed commented code in view.py

--- polls/views.py
+++ polls/views.py
@@ -5,44 +5,6 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Choice, Question
-
-# implement views the hard way.
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {"latest_question_list": latest_question_list}
-#     return render(request, "polls/index.html", context)
-
-# def details(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/details.html", {"question": question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/results.html", {"question": question})
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-#     except (KeyError, Choice.DoesNotExist):
-#         #Redisplay the question voting form.
-#         return render(
-#             request,
-#             "polls/details.html",
-#             {
-#                 "question": question,
-#                 "error_message": "You didn't select a choice.",
-#             },
-#         )
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always retunr a HttpResponseRedirect after successfully dealing with POST data.  
-#         # This prevent data from being posted twice if a user hits the Back button.
-
-#         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
