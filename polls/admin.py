e8844ff74992e1f755703367854868ae06e7dbfb
mejja <majorkiema.mk@gmail.com>
2023-11-05 20:24:04 +0300

    modified the admin site to include filter and search capabilities

--- polls/admin.py
+++ polls/admin.py
@@ -3,11 +3,19 @@ from .models import Question, Choice
 
 
 # Register your models here
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("date information", {"fields": ["pub_date"]}),
     ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    #Adding a filter sidebar
+    list_filter = ["pub_date"]
+    #adding Searching capabilities
+    search_fields = ["question_text"]
+    inlines = [ChoiceInline]
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib import admin
+
 # Create your models here.
 #Question model
 class Question(models.Model):
@@ -9,6 +11,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+         #add display decorator to  add attribute to our display
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+     )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
