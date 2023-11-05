4dbcb256d4227d2b199a2458254bad31ddf07607
mejja <majorkiema.mk@gmail.com>
2023-11-05 19:49:56 +0300

    splitted the question fieldset

--- polls/admin.py
+++ polls/admin.py
@@ -6,5 +6,8 @@ from .models import Question, Choice
 admin.site.register(Choice)
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("date information", {"fields": ["pub_date"]}),
+    ]
 admin.site.register(Question, QuestionAdmin)
