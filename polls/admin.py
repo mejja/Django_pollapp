6c3fcaca22a2d1d280c5ed3349073f255be1b497
mejja <majorkiema.mk@gmail.com>
2023-11-07 21:29:53 +0300

    updated admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -10,7 +10,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     save_on_top = False
     fieldsets = [
-        (None, {"fields": ["question_text"]}),
+        ("Question Information", {"fields": ["question_text"]}),
         ("date information", {"fields": ["pub_date"]}),
     ]
     list_display = ["question_text", "pub_date", "was_published_recently"]
